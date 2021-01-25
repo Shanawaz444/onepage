9b3164851e4b7b7af5e484bec6f6add4691ffaad
Shanawaz444 <shanawaz0891@gmail.com>
2021-01-26 01:43:42 +0530

    created education componengts

---
+++ src/app/about-education/about-education.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-education',
+  templateUrl: './about-education.component.html',
+  styleUrls: ['./about-education.component.scss']
+})
+export class AboutEducationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
