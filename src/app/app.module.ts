422118f17ac639d1fb165f8b7301dae40678b0f4
Shanawaz444 <shanawaz0891@gmail.com>
2021-07-26 06:43:08 +0530

    updating to v11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { IgxBannerModule } from 'igniteui-angular';
-import 'hammerjs';
-import { from } from 'rxjs';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
-import { BannerComponent } from './banner/banner.component';
-import { LanguagesihaveworkedComponent } from './languagesihaveworked/languagesihaveworked.component';
-import { MyeducationComponent } from './myeducation/myeducation.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { AboutEducationComponent } from './about-education/about-education.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    AboutComponent,
-    BannerComponent,
-    LanguagesihaveworkedComponent,
-    MyeducationComponent,
-    ProjectsComponent,
-    AboutEducationComponent,
+    AppComponent
   ],
   imports: [
-    IgxBannerModule,
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    FlexLayoutModule,
-    MatListModule,
-    MatCardModule,
-    MatGridListModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
