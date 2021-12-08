8d625d250ea0af23de24a03eac7618887dec34a1
Shanawaz444 <shanawaz0891@gmail.com>
2021-12-08 21:17:22 +0530

    added the functinality for the ng for dericitve

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,9 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-  
-  
 
+  contacticons:any=[
+    {
+      "className":"github",
+      "href":"https://github.com/Shanawaz444",
+      "src":"assets/images/github.png"
+    },
+    {
+      "className":"linkedin",
+      "href":"https://www.linkedin.com/in/shanawaz444/",
+      "src":"assets/images/linkedin.png"
+    },
+    {
+      "className":"gmail",
+      "href":"shanawaz0891@gmail.com",
+      "src":"assets/images/gmail.png"
+    },
+    {
+      "className":"instagram",
+      "href":"https://www.instagram.com/shan_444_/",
+      "src":"assets/images/instagram.png"
+    },
+  ];
 
   constructor() { } 
 ngOnInit(): void {
